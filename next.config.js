825f370f2bb7c9c4eb14656186ae0cbd8947e73c
RuGe <70239531+ruge0326@users.noreply.github.com>
2023-03-06 18:09:03 -0600

    wip: deploy netlify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/* eslint-disable no-unused-vars */
+const path = require("path");
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "./src/assets/scss")],
+  },
+  images: {
+    domains: ["ordinals.com", "d2v3k2do8kym1f.cloudfront.net"],
+  },
+
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // eslint-disable-next-line no-param-reassign
+    (config.experiments = { asyncWebAssembly: true }),
+      (config.ignoreWarnings = [
+        {
+          message:
+            /(magic-sdk|@walletconnect\/web3-provider|@web3auth\/web3auth)/,
+        },
+      ]);
+    return config;
+  },
+});
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+
+                    <script
+                        src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
+                        integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D"
+                        crossOrigin="anonymous"
+                        async
+                    />
+                </body>
+            </Html>
+        );
+    }
 }
+
+export default MyDocument;
